3ed15fd6bbbca3fbf44ae63ac341c8810d8fc9d9
xdevore <xdevore@haverford.edu>
2023-04-27 01:10:12 -0400

    created 3 onevsone models for each genre combo

--- code/model_onevsone.py
+++ code/model_onevsone.py
@@ -31,47 +31,46 @@ for i in range(len(genre_list)):
             if other_genre in item:
                 other_genre_specs_folder = item
 
-        train_generator = custom_generator_onevsone(data_path, target_size=(64, 192), batch_size=batch_size, subset="training", specified_genre_train_folder=specified_genre_specs_folder, other_genre_train_folder=other_genre_specs_folder)
-        validation_generator = custom_generator_onevsone(data_path, target_size=(64, 192), batch_size=batch_size, subset="validation", specified_genre_train_folder=specified_genre_specs_folder, other_genre_train_folder=other_genre_specs_folder)
+        for k in range(3):
+            train_generator = custom_generator_onevsone(data_path, target_size=(64, 192), batch_size=batch_size, subset="training", specified_genre_train_folder=specified_genre_specs_folder, other_genre_train_folder=other_genre_specs_folder)
+            validation_generator = custom_generator_onevsone(data_path, target_size=(64, 192), batch_size=batch_size, subset="validation", specified_genre_train_folder=specified_genre_specs_folder, other_genre_train_folder=other_genre_specs_folder)
 
+            other_genres_count = len(os.listdir(os.path.join(data_path, other_genre_specs_folder)))
+            specified_genre_count = len(os.listdir(os.path.join(data_path, specified_genre_specs_folder)))
 
-        other_genres_count = len(os.listdir(os.path.join(data_path, other_genre_specs_folder)))
-        specified_genre_count = len(os.listdir(os.path.join(data_path, specified_genre_specs_folder)))
+            print(other_genres_count)
+            print(specified_genre_count)
 
-        print(other_genres_count)
-        print(specified_genre_count)
+            train_steps_per_epoch = (specified_genre_count + other_genres_count) // (batch_size * 2)
+            validation_steps = train_steps_per_epoch // 5
 
-        train_steps_per_epoch = (specified_genre_count + other_genres_count) // (batch_size * 2)
-        validation_steps = train_steps_per_epoch // 5
+            model = Sequential()
+            model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(64, 192, 3)))
+            model.add(MaxPooling2D(pool_size=(2, 2)))
+            model.add(Conv2D(64, (3, 3), activation='relu'))
+            model.add(MaxPooling2D(pool_size=(2, 2)))
+            model.add(Conv2D(128, (3, 3), activation='relu'))
+            model.add(MaxPooling2D(pool_size=(2, 2)))
+            model.add(Dropout(0.5))
+            model.add(Flatten())
+            model.add(Dense(128, activation='relu'))
+            model.add(Dropout(0.5))
+            model.add(Dense(1, activation='sigmoid'))
 
-        model = Sequential()
-        model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(64, 192, 3)))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Conv2D(64, (3, 3), activation='relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Conv2D(128, (3, 3), activation='relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Dropout(0.5))
-        model.add(Flatten())
-        model.add(Dense(128, activation='relu'))
-        model.add(Dropout(0.5))
-        model.add(Dense(1, activation='sigmoid'))
+            model.compile(optimizer=Adam(learning_rate=0.0001), loss='binary_crossentropy', metrics=['accuracy'])
 
-        model.compile(optimizer=Adam(learning_rate=0.0001), loss='binary_crossentropy', metrics=['accuracy'])
+            model_name = specified_genre + '_vs_' + other_genre + '_best_model_' + str(k) + '.h5'
+            checkpoint = ModelCheckpoint(model_name, save_best_only=True, monitor='val_accuracy', mode='max', verbose=1)
 
-        model_name = specified_genre + '_vs_' + other_genre + '_best_model.h5'
+            history = model.fit(
+                train_generator,
+                steps_per_epoch=train_steps_per_epoch,
+                validation_data=validation_generator,
+                validation_steps=validation_steps,
+                epochs=11,
+                callbacks=[checkpoint]
+            )
 
-        checkpoint = ModelCheckpoint(model_name, save_best_only=True, monitor='val_accuracy', mode='max', verbose=1)
+            model_name = specified_genre + '_vs_' + other_genre + str(k) + '_genre_classifier.h5'
 
-        history = model.fit(
-            train_generator,
-            steps_per_epoch=train_steps_per_epoch,
-            validation_data=validation_generator,
-            validation_steps=validation_steps,
-            epochs=11,
-            callbacks=[checkpoint]
-        )
-
-        model_name = specified_genre + '_vs_' + other_genre + '_genre_classifier.h5'
-
-        model.save(model_name)
+            model.save(model_name)
